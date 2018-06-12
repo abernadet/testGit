6733852d13581fcb6de637805c469ae916ff79c0
Arthur BERNADET <abernadet@PHILO.INTRA>
2018-06-12 09:38:18 +0200

    création de la page contact, modif de l'index

--- contact.php
+++ contact.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Titre</title>
+    <title>Contact</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
     <script src="main.js"></script>
 </head>
 <body>
-    <h1>Page d'acceuil V2</h1>
-    ocoucou
+    <h1>Contact</h1>
+    <form action=""></form>
 </body>
 </html>
